390684bc333424a3e68a72a98409c59c81c29323
andrey.kutin <andrey.k@itechpsp.com>
2023-01-29 23:01:50 +0300

    Initial commit

---
+++ src/main/java/com/n0tm/tripmoneyser/TripMoneyserApplication.java
@@ -0,0 +1,13 @@
+package com.n0tm.tripmoneyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripMoneyserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripMoneyserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/n0tm/tripmoneyser/TripMoneyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n0tm.tripmoneyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripMoneyserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
